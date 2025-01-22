a97f3dd7a58e36507b032c9c8bae13499260df02
Iskandar <134040429+Iskandar1211@users.noreply.github.com>
2025-01-22 23:11:38 +0500

    created pagination.component.tsx, used into posts.component.tsx

---
+++ src/components/pagination/pagination.component.tsx
@@ -0,0 +1,79 @@
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious
+} from "@/components/ui/pagination.tsx";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select.tsx";
+import {PostType} from "@/types";
+import {useSearchParams} from "react-router-dom";
+import {Dispatch, SetStateAction} from "react";
+
+const PaginationComponent = ({posts, postsPerPage, setPostsPerPage}: {
+  posts: PostType[],
+  postsPerPage: number,
+  setPostsPerPage: Dispatch<SetStateAction<number>>
+}) => {
+  const totalPages = posts?.length ? Math.ceil(posts.length / postsPerPage) : 0;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const postId = parseInt(searchParams.get("postId") || "1", 10);
+
+  const handlePageChange = (page: number) => {
+    setSearchParams({postId: String(page)});
+  };
+  const handlePostsPerPageChange = (value: number) => {
+    setPostsPerPage(value);
+    setSearchParams({postId: "1"});
+  };
+
+  return (
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem className={'cursor-pointer'}>
+          <PaginationPrevious
+            onClick={() => handlePageChange(postId > 1 ? postId - 1 : 1)}
+          />
+        </PaginationItem>
+        <PaginationItem>
+          <Select value={String(postsPerPage)} onValueChange={(event) => handlePostsPerPageChange(parseInt(event))}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Select per page"/>
+            </SelectTrigger>
+            <SelectContent className={'bg-white'}>
+              <SelectGroup>
+                <SelectLabel>Posts per page</SelectLabel>
+                <SelectItem value="10">10 posts per page</SelectItem>
+                <SelectItem value="20">20 posts per page</SelectItem>
+                <SelectItem value="50">30 posts per page</SelectItem>
+              </SelectGroup>
+            </SelectContent>
+          </Select>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink
+            isActive={true}
+          >
+            {postId}
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem className={'cursor-pointer'}>
+          <PaginationNext
+            onClick={() => handlePageChange(postId < totalPages ? postId + 1 : totalPages)}
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+  );
+};
+
+export default PaginationComponent;
--- src/components/posts/posts.component.tsx
+++ src/components/posts/posts.component.tsx
@@ -2,65 +2,36 @@ import {getPosts} from "@/rest-api";
 import {useQuery} from "@tanstack/react-query";
 import {PostType} from "@/types";
 import PostComponent from "@/components/posts/post.component.tsx";
-import {
-  Pagination,
-  PaginationContent,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious
-} from "@/components/ui/pagination.tsx";
 import {useSearchParams} from "react-router-dom";
+import {useState} from "react";
+import PaginationComponent from "@/components/pagination/pagination.component.tsx";
 
 const PostsComponent = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+
+  const [searchParams] = useSearchParams();
   const postId = parseInt(searchParams.get("postId") || "1", 10);
   const {data: posts} = useQuery<PostType[]>({queryKey: ['posts'], queryFn: getPosts});
-  const postsPerPage = 10;
-
+  const [postsPerPage, setPostsPerPage] = useState(10);
   const indexOfLastPost = postId * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts?.slice(indexOfFirstPost, indexOfLastPost);
 
-  const totalPages = posts?.length ? Math.ceil(posts.length / postsPerPage) : 0;
-
-  const handlePageChange = (page: number) => {
-    setSearchParams({postId: String(page)});
-  };
-
-  if(!posts) return <div>No posts available</div>
+  if (!posts) return <div>No posts available</div>
 
   return (
     <div className={'container mx-auto flex flex-col h-screen'}>
-      <div className={'grid grid-cols-4 p-5 gap-4 flex-1 border'}>
+      <div className={'grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 p-5 gap-4 flex-1'}>
         {currentPosts && currentPosts.length > 0 && currentPosts.map(post => {
           return <PostComponent key={post.id} post={post}/>
         })}
       </div>
 
-      <div className={'col-span-4 border'}>
-        {posts && <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious
-                onClick={() => handlePageChange(postId > 1 ? postId - 1 : 1)}
-              />
-            </PaginationItem>
-                <PaginationItem >
-                  <PaginationLink
-                    isActive={true}
-                  >
-                    {postId}
-                  </PaginationLink>
-                </PaginationItem>
-
-            <PaginationItem>
-              <PaginationNext
-                onClick={() => handlePageChange(postId < totalPages ? postId + 1 :totalPages)}
-              />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>}
+      <div className={'col-span-4 sticky bottom-0 py-2 bg-white'}>
+        <PaginationComponent
+          postsPerPage={postsPerPage}
+          posts={posts}
+          setPostsPerPage={setPostsPerPage}
+        />
       </div>
     </div>
   );
