555c8c10221cba5de6546c6041bcf5f6db186d02
Iskandar <134040429+Iskandar1211@users.noreply.github.com>
2025-01-23 00:56:51 +0500

    feat: added search to title

--- src/components/posts/posts.component.tsx
+++ src/components/posts/posts.component.tsx
@@ -3,12 +3,16 @@ import {useQuery} from "@tanstack/react-query";
 import {PhotosType, PostType} from "@/types";
 import PostComponent from "@/components/posts/post.component.tsx";
 import {useSearchParams} from "react-router-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import PaginationComponent from "@/components/pagination/pagination.component.tsx";
+import {Input} from "@/components/ui/input.tsx";
+import {Button} from "@/components/ui/button.tsx";
 
 const PostsComponent = () => {
   const [searchParams] = useSearchParams();
   const postId = parseInt(searchParams.get("postId") || "1", 10);
+  const [postTitle, setPostTitle] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState<PostType[]>([]);
 
   const {data: posts} = useQuery<PostType[]>({queryKey: ['posts'], queryFn: getPosts});
   const {data: photos} = useQuery<PhotosType[]>({queryKey: ['photos'], queryFn: getPhotos});
@@ -16,16 +20,31 @@ const PostsComponent = () => {
   const [postsPerPage, setPostsPerPage] = useState(10);
   const indexOfLastPost = postId * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts?.slice(indexOfFirstPost, indexOfLastPost).map(post => ({
+  const currentPosts = filteredPosts?.slice(indexOfFirstPost, indexOfLastPost).map(post => ({
     ...post,
     photos: photos?.filter(photo => photo.albumId === post.id).slice(0, 3)
   }));
-  console.log("currentPosts",currentPosts)
+
+  useEffect(() => {
+    if (!posts) return
+    setFilteredPosts(posts)
+  }, [posts])
+
+  const filteredPostsByName = () => {
+    if (!posts) return
+    const filteredPosts = posts.filter(post => post.title.toLowerCase().includes(postTitle.toLowerCase()))
+    setFilteredPosts(filteredPosts)
+  }
+
   if (!posts) return <div>No posts available</div>
 
   return (
     <div className={'container mx-auto flex flex-col h-screen'}>
       <h1 className={'text-2xl font-bold text-center'}>Posts</h1>
+      <div className={'flex gap-2 w-fit'}>
+        <Input placeholder={'Search...'} className={'rounded'} onChange={(event) => setPostTitle(event.target.value)}/>
+        <Button onClick={() => filteredPostsByName()}>Search</Button>
+      </div>
       <div className={'grid grid-cols-4 max-xl:grid-cols-3 max-lg:grid-cols-2 max-sm:grid-cols-1 p-5 gap-4 flex-1'}>
         {currentPosts && currentPosts.length > 0 && currentPosts.map(post => {
           return <PostComponent key={post.id} post={post}/>
